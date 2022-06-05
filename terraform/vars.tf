4f753f5f91f302b08dfc13b9114d4ffb06ea5468
Thiago Nelson <thiagonelson@gmail.com>
2022-06-05 12:48:37 +0100

    Keys

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,3 +2,16 @@ variable "region" {
   default     = "eu-west-3"
   description = "AWS region"
 }
+
+variable "ssh_keys" {
+  type = string
+  description = "Public key to export from local to AWS"
+  sensitive = true
+}
+
+variable "pvt_key" {
+  type = string
+  default = "~/.ssh/id_aws_challenge"
+  description = "(Ansible) Path to private key, to maintain servers."
+  sensitive = true
+}
