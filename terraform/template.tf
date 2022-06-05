fbb83b1dd00f90da29a19a543c8e27daa4ed85d7
Thiago Nelson <thiagonelson@gmail.com>
2022-06-05 18:22:42 +0100

    generate inventory to ansible: loadbalance

--- terraform/template.tf
+++ terraform/template.tf
@@ -1,7 +1,9 @@
 data "template_file" "inventory" {
     template = file("./templates/infra.tpl")
     vars = {
-        lb_ip = "${join("\n", [for instance in aws_instance.loadbalance : join("", [instance.ipv4_address, " internal_ip=", instance.ipv4_address_private, " ansible_ssh_private_key_file=", var.pvt_key])])}"
+        lb_ip = "${join("", [aws_instance.loadbalance.public_ip," internal_ip=", aws_instance.loadbalance.private_ip, " ansible_ssh_private_key_file=", var.pvt_key])}"
+        db_rds_ip = ""
+        k8s_node_ip = ""
     }
 }
 
---
+++ terraform/templates/infra.tpl
@@ -0,0 +1,10 @@
+[lb]
+${lb_ip}
+
+[db]
+${db_rds_ip}
+
+[nodes]
+${k8s_node_ip}
+
+
