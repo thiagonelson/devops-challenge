94932ef609f310eb60d7c7e4e94b016508d4b757
Thiago Nelson <thiagonelson@gmail.com>
2022-06-05 18:23:54 +0100

    output: LB IPs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,19 @@
 output "cluster_name" {
-description = "K8s Cluster Name:"
-value = local.cluster_name 
+   description = "K8s Cluster Name:"
+   value       = local.cluster_name 
+}
+
+output "region" {
+  description = "Region:"
+  value       = var.region
+}
+
+output "loadbalance" {
+  description = "Loadbalance Private IP"
+  value       = aws_instance.loadbalance.private_ip
+}
+
+output "loadbalance_pub"
+  description = "Loadbalance Public IP"
+  value       = aws_instance.loadbalance.public_ip
 }
